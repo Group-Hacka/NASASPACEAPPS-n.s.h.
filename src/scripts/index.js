e2b295477db9882cfe1c3bfe2fc8da01a579748b
jessicamelise <jessica.melise@hotmail.com>
2020-05-30 22:50:40 -0300

    atualização html css mobile

---
+++ src/scripts/index.js
@@ -0,0 +1,12 @@
+const menuBar = document.getElementById("menu-bar");
+const showMenu = document.getElementById("show-menu");
+ 
+
+const menuExhibit = () => {
+    showMenu.classList.toggle("menu-exhibit");
+}
+
+menuBar.addEventListener("click", () =>{
+    menuExhibit();
+    console.log("ola")
+});
